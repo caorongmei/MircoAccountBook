413783251f08aee0ce1e3a5c5de770dae95ecd02
caorongmei <hcaorm@zetyun.com>
2017-07-12 18:09:16 +0800

    add

---
+++ PythonStudy/StudyUp/Socket.py
@@ -0,0 +1,22 @@
+#coding=utf-8
+
+import socket
+
+#创建socket对象
+s=socket.socket()
+host=socket.gethostname()
+port=12345
+s.bind((host,port))
+s.listen(5)
+
+while True:
+    c,addr=s.accept()
+    print '连接地址：',addr
+    c.send('欢迎~~')
+    c.close()
+
+
+
+
+
+
